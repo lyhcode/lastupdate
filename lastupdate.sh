6323f4429c2f39998f3b287942cb4360d74a7733
林彥宏 <kyle@Kyle-MacBook-Air.local>
2021-06-02 16:07:03 +0800

    add NOPASSWD for sudoers

--- lastupdate.sh
+++ lastupdate.sh
@@ -3,7 +3,35 @@
 # Setup script for setting up a new macos machine
 echo "Starting lastupdate"
 
-echo "Install command line developer tools"
-xcode-select --install
+## Setup /etc/sudoers for sudo without password prompt
+echo "Setup NOPASSWD for %staff"
+sudo grep -q '^%staff' /etc/sudoers || sudo sed -i '' 's/^%admin.*/&\n%staff          ALL = (ALL) NOPASSWD: ALL/' /etc/sudoers
 
+## Command Line Tools for Xcode
+# echo "Install command line developer tools"
+# xcode-select --install
+# xcode-select -p &> /dev/null
+# if [ $? -ne 0 ]; then
+#   echo "Xcode CLI tools not found. Installing them..."
+#   touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+#   PROD=$(softwareupdate -l |
+#     grep "\*.*Command Line" |
+#     head -n 1 | awk -F"*" '{print $2}' |
+#     sed -e 's/^ *//' |
+#     tr -d '\n')
+#   softwareupdate -i "$PROD" -v;
+# else
+#   echo "Xcode CLI tools OK"
+# fi
 
+## Homebrew
+echo "Install Homebrew"
+CI=1; /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+## Vagrant
+brew install vagrant
+
+## Speedtest CLI
+brew tap teamookla/speedtest
+brew update
+brew install speedtest --force
